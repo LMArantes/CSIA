1b3c0c8f231a808803dad4fb657a29a66f6245d1
Lucas Arantes <93750203+LMArantes@users.noreply.github.com>
2024-10-02 10:43:59 -0400

    Create figures.h

---
+++ Lab_5/figures/figures.h
@@ -0,0 +1,9 @@
+#ifndef figures_hpp
+#define figures_hpp
+
+void filledSquare(int size, char paintChar);
+void hollowSquare(int size, char paintChar);
+void forwardSlash(int size, char paintChar);
+void backSlash(int size, char paintChar);
+void drawX(int size, char paintChar);
+#endif
